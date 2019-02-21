43d51fe89e207844bd8d7108f4aad702cc50220c
TheMingH <544474829@qq.com>
2019-02-21 14:52:35 +0800

    Initial commit.

--- mybatisPlus/mybatisPlusChild/src/main/java/org/child/generate/CodeGeneration.java
+++ mybatisPlus/mybatisPlusChild/src/main/java/org/child/generate/CodeGeneration.java
@@ -8,72 +8,59 @@ import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
-
 /**
- * 
- * @ClassName: CodeGeneration
- * @Description: 代码生成器
- * @date 2019/1/8
- */
+ *  *   * @ClassName: CodeGeneration  * @Description: 代码生成器  * @date 2019/1/8  
+ */
 public class CodeGeneration {
-      public static void main(String[] args) {
-AutoGenerator mpg = new AutoGenerator();
+	public static void main(String[] args) {
+		AutoGenerator mpg = new AutoGenerator();
 
+		GlobalConfig gc = new GlobalConfig();
+		// 代码生成的位置
+		gc.setOutputDir("D://CodeGeneration-master/mybatisPlus/mybatisPlusChild/src/main/java");
+		gc.setFileOverride(true);
+		gc.setActiveRecord(false);// 不需要ActiveRecord特性的请改为false
+		gc.setEnableCache(false);// XML 二级缓存
+		gc.setBaseResultMap(true);// XML ResultMap
+		gc.setBaseColumnList(false);// XML columList
+		gc.setAuthor("Walle");// 作者
 
+		// 自定义文件命名，注意 %s 会自动填充表实体属性！
+		gc.setControllerName("%sController");
+		gc.setServiceName("%sService");
+		gc.setServiceImplName("%sServiceImpl");
+		gc.setMapperName("%sMapper");
+		gc.setXmlName("%sMapper");
+		mpg.setGlobalConfig(gc);
 
- GlobalConfig gc = new GlobalConfig();
- gc.setOutputDir("E://myproject/mybatisPlus/mybatisPlusChild/src/main/java");
-gc.setFileOverride(true);
- gc.setActiveRecord(false);// 不需要ActiveRecord特性的请改为false
- gc.setEnableCache(false);// XML 二级缓存
- gc.setBaseResultMap(true);// XML ResultMap
- gc.setBaseColumnList(false);// XML columList
- gc.setAuthor("Walle");// 作者
+		DataSourceConfig dsc = new DataSourceConfig();
+		dsc.setDbType(DbType.MYSQL);
+		dsc.setDriverName("com.mysql.jdbc.Driver");
+		dsc.setUsername("root");
+		dsc.setPassword("123456");
+		dsc.setUrl("jdbc:mysql://localhost:3306/test");
+		mpg.setDataSource(dsc);
 
+		StrategyConfig strategy = new StrategyConfig();
+		strategy.setTablePrefix(new String[] { "itcast_" });// 此处可以修改为您的表前缀
+		strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+		strategy.setInclude(new String[] { "u_test" }); // 需要生成的表
 
- // 自定义文件命名，注意 %s 会自动填充表实体属性！
- gc.setControllerName("%sController");
- gc.setServiceName("%sService");
- gc.setServiceImplName("%sServiceImpl");
- gc.setMapperName("%sMapper");
-gc.setXmlName("%sMapper");
-mpg.setGlobalConfig(gc);
+		strategy.setSuperServiceClass(null);
+		strategy.setSuperServiceImplClass(null);
+		strategy.setSuperMapperClass(null);
 
+		mpg.setStrategy(strategy);
 
-
- DataSourceConfig dsc = new DataSourceConfig();
- dsc.setDbType(DbType.MYSQL);
- dsc.setDriverName("com.mysql.jdbc.Driver");
-dsc.setUsername("root");
- dsc.setPassword("123456");
- dsc.setUrl("jdbc:mysql://localhost:3306/test");
- mpg.setDataSource(dsc);
-
-
-
- StrategyConfig strategy = new StrategyConfig();
- strategy.setTablePrefix(new String[] { "itcast_" });// 此处可以修改为您的表前缀
- strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
- strategy.setInclude(new String[] { "u_test" }); // 需要生成的表
-
-
-strategy.setSuperServiceClass(null);
- strategy.setSuperServiceImplClass(null);
- strategy.setSuperMapperClass(null);
-
-
- mpg.setStrategy(strategy);
-
-
-PackageConfig pc = new PackageConfig();
- pc.setParent("org.child");
- pc.setController("controller");
-pc.setService("service");
- pc.setServiceImpl("serviceImpl");
- pc.setMapper("mapper");
- pc.setEntity("entity");
- pc.setXml("xml");
- mpg.setPackageInfo(pc);
- mpg.execute();
-}
+		PackageConfig pc = new PackageConfig();
+		pc.setParent("org.child");
+		pc.setController("controller");
+		pc.setService("service");
+		pc.setServiceImpl("serviceImpl");
+		pc.setMapper("mapper");
+		pc.setEntity("entity");
+		pc.setXml("xml");
+		mpg.setPackageInfo(pc);
+		mpg.execute();
+	}
 }
